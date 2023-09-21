68a4db261fef80333fdc02b6d2303360dbfa882f
daffydbest7 <daffydbest@gmail.com>
2023-09-21 10:37:40 +0100

    added image

--- src/App.js
+++ src/App.js
@@ -1,31 +1,33 @@
 import './App.css';
 import { AiOutlinePhone, AiOutlineMail, AiOutlineLinkedin, AiOutlineWhatsApp, AiFillFacebook} from "react-icons/ai";
 import avatar from "./asset/avatar.png";
+import nabena_ from "./asset/nabena_.jpg";
 //import ReactDOM from 'react-dom';
 function App() {
   
   return (
-    <div className="App text-white">
+    <div className="App text-white fixed">
+      <div className='container '>
       <div className='container  min-h-screen'>
-        <section className='container border border-gray-500 rounded-lg mt-[80px]'>
+        <section className='container border-2 border-gray-500 rounded-lg mt-[80px]'>
           {/**image */}
           <div className='mx-auto text-center'>
-          <div className='mx-auto rounded-lg'> <img src={avatar} alt='avatar' className='rounded-lg mx-auto text-center max-w-[100px] mt-[-60px]' /></div>
-          <h2 className='mx-auto font-bold text-2xl text-teal-500'> EBIBOWEI NABENA </h2>
+          <div className='mx-auto rounded-lg'> <img src={nabena_} alt='avatar' className='rounded-lg mx-auto text-center max-w-[100px] mt-[-60px] hover:scale-110 duration-200 shadow-lg' /></div>
+          <h2 className='mx-auto font-bold text-2xl text-blue-500 mt-4'> EBIBOWEI NABENA </h2>
           <p className='mx-auto text-center'> I am a Data Analyst passionate about transforming complex data into actionable insights that drive
           positive outcomes and operational efficiency</p>
           </div>
          
            {/**phone and Email */}
-           <div className='mt-8'>
+           <div className='mt-8 ml-4'>
            <div>
-           <div className='flex flex-row space-x-2 items-center text-teal-500'>
+           <div className='flex flex-row space-x-2 items-center text-blue-500'>
             <span><AiOutlinePhone/></span> <span className='font-bold '>Phone Number</span>
            </div>
             +234 (0) 906-850-9555
            </div>
            <div className='mt-4 mb-4'>
-           <div className='flex flex-row space-x-2 items-center text-teal-500'>
+           <div className='flex flex-row space-x-2 items-center text-blue-500'>
             <span><AiOutlineMail/></span> <span className='font-bold'>Email Address</span>
            </div>
             ebibowei.nabena@gmail.com 
@@ -33,15 +35,15 @@ function App() {
            </div>
         </section>
        {/**socials */}
-       <section className='container mt-[80px] '>
+       <section className='container mt-[50px]'>
          <div className=''>
          <h2 className='font-bold text-center text-2xl'> SOCIAL MEDIA </h2>
            
-           <div className='mt-8 border border-lg border-gray-500 rounded-lg'>
-            <div className='flex flex-row space-x-12 my-8 text-teal-500'>
-            <a   href="https://www.linkedin.com/in/ebibowei-nabena"  rel="noreferrer" target='_blank' className='font-bold cursor-pointer hover:text-blue-500'> <AiOutlineLinkedin size={25}/> </a >
-            <a  href="https://facebook.com/iammrhaitch"  rel="noreferrer" target='_blank' className='font-bold cursor-pointer'><AiFillFacebook size={25}/></a>
-            <a  href="https://wa.me/+2349068509555"  rel="noreferrer" target='_blank' className='font-bold  cursor-pointer'><AiOutlineWhatsApp size={25}/></a>
+           <div className='mt-8 border-2 border-lg border-gray-500 rounded-lg '>
+            <div className='flex flex-row space-x-12 justify-around my-8 text-blue-500'>
+            <a   href="https://www.linkedin.com/in/ebibowei-nabena"  rel="noreferrer" target='_blank' className='font-bold cursor-pointer animate-pulse'> <AiOutlineLinkedin size={25}/> </a >
+            <a  href="https://facebook.com/iammrhaitch"  rel="noreferrer" target='_blank' className='font-bold cursor-pointer animate-pulse'><AiFillFacebook size={25}/></a>
+            <a  href="https://wa.me/+2349068509555"  rel="noreferrer" target='_blank' className='font-bold  cursor-pointer animate-pulse'><AiOutlineWhatsApp size={25}/></a>
            </div>
            </div>
          </div>
@@ -49,6 +51,7 @@ function App() {
            
         </section>
        </div>
+      </div>
     </div>
   );
 }
